df10feea0075b7373a49e43f4bcb17b040c4db24
mathiasare <mathias.are@ridango.com>
2023-04-09 22:10:02 +0300

    improved main.py

--- kood/chatbot-workshop/src/main.py
+++ kood/chatbot-workshop/src/main.py
@@ -9,17 +9,18 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 # Seame juturoboti õigused
 intents = intents=discord.Intents.default()
-# intents.message_content = True
+intents.message_content = True
 
 # Loome discordiga suhtlemise "client" objekti
 client = discord.Client(intents = intents)
 
+####################################################
 # Event listener meetodid, sündmustele reageerimiseks
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
-
+    print(f'Tere, maailm! Minu nimi on {client.user}.')
+    
+    
 ####################################################
 ######## LISA SIIA on_message() FUNKTSIOON #########
 
